8f7b0e9d024ae159589446f248d4532d1990b237
Sean Davis <seandavi@gmail.com>
2019-08-08 11:32:47 -0400

    add stacked variant call data and accessor

---
+++ R/variant_calls.R
@@ -0,0 +1,12 @@
+#' load stacked, filtered mutect calls
+#'
+#' @importFrom readr read_tsv
+#'
+#' @export
+variant_calls = function() {
+  .variant_call_file = system.file(package='TargetOsteoAnalysis',
+                                   'extdata/summary.TargetOsteo.mutect.GATKPlus.sampleInfo.geneLists.tab.gz')
+
+  dat = readr::read_tsv(.variant_call_file)
+  return(dat)
+}
